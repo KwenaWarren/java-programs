d211f62e1fee78651d6f383053bc197e02e26c62
Warren Mongatane <warrenmongatane@gmail.com>
2021-04-01 12:09:40 +0200

    Update HelloWorld.java

--- src/Basics/HelloWorld.java
+++ src/Basics/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main (String[] args) {
 		System.out.println("Hello Kwena");
 		System.out.println("Testing Git-Hub");
+		System.out.println("Adding code from Git-Hub");
 	}
 
 }
